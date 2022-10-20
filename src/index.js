76c58202bbecff1c275b3b60fc67deb5a34ffe95
Vanessa O <vaneolipa@gmail.com>
2022-10-20 02:40:16 -0500

    Modify files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import addScore from './modules/addScore.js';
+import Interface from './modules/interface';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', addScore);
+
+window.addEventListener('load', () => {
+  
+  Interface.displayScore();
+});
+
---
+++ src/modules/score.js
@@ -0,0 +1,8 @@
+class Score {
+    constructor(userName, userScore){
+      this.userName = userName;
+      this.userScore = userScore;
+    }
+  }
+
+  export default Score;
